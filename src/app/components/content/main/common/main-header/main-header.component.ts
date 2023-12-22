0311ae13c236b9d3d1b3d22bc17b677b4764564a
rafaeljgomezg <rafaeljgomezg@gmail.com>
2023-12-21 22:59:29 -0500

    main-header-toolbar change

--- src/app/components/content/main/common/main-header/main-header.component.ts
+++ src/app/components/content/main/common/main-header/main-header.component.ts
@@ -1,10 +1,20 @@
 import { Component, signal } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+
 import { ImgItem } from '../../../../../models/images';
 
 @Component({
   selector: 'app-main-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDividerModule
+  ],
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.scss'
 })
